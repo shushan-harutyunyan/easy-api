db1dd98774484138862d417833399e5dba56133f
shrutyunyan <shushan.harutyunyan8@gmail.com>
2025-04-06 20:40:45 +0400

    Initial Commit

---
+++ src/test/java/epam/easyapi/EasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.easyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
