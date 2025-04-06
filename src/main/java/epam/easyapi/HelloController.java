0cfe893f61942730472bce2fc0651abc584b2f7e
shrutyunyan <shushan.harutyunyan8@gmail.com>
2025-04-06 20:49:07 +0400

    Change message

--- src/main/java/epam/easyapi/HelloController.java
+++ src/main/java/epam/easyapi/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Welcome to my back end application";
     }
 }
